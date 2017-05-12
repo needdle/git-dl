ce345f2e1b5e754ef8a53db227858f9f2e8959c1
Yijun Yu <yijun.yu@gmail.com>
2017-05-12 15:49:45 +0800

    performance: ignore if a diff record is too big (longer than 1MB)

--- src/gitlog.cc
+++ src/gitlog.cc
@@ -153,6 +153,176 @@ void process_hunk_text(fast::Log_Commit_Diff_Hunk *hunk, std::string text) {
 	} while (linePos != std::string::npos);
 }
 
+void commit(fast::Log_Commit * current_commit, std::string diff) {
+	if (diff.length() > 1000000) {
+		cout << "Ignoring the diff block because it is too big: "<< diff.length() << endl;
+		return;
+	}
+	std::string file_a;
+	std::string file_b;
+	std::string diff_line;
+	if (current_commit != NULL) {
+		cout << "." << flush;
+		fast::Log_Commit_Diff_Hunk * hunk = NULL;
+		fast::Log_Commit_Diff * diff_record = NULL;
+		size_t linePos = std::string::npos;
+		std::string hunk_text = "";
+		int lineno = 0;
+		do {
+		    linePos = diff.find("\n");
+		    if (linePos != std::string::npos) {
+			diff_line = diff.substr(0, linePos);
+			diff = diff.substr(linePos + 1);
+			bool is_special = false;
+			size_t prefixPos = diff_line.find(DIFF_PREFIX);
+			if (prefixPos != std::string::npos) {
+			    std::string diff_prefix= diff_line.substr(0, prefixPos);
+			    if (diff_prefix == "") {
+				diff_line = diff_line.substr(prefixPos + strlen(DIFF_PREFIX));
+				size_t fieldPos = diff_line.find(" b/");
+				file_a = diff_line.substr(2, fieldPos - 2);
+				file_b = diff_line.substr(fieldPos+3);
+				diff_record = current_commit->add_diff();
+				diff_record->set_a(file_a);
+				diff_record->set_b(file_b);
+				bool is_code = false;
+				size_t extPos = file_a.rfind(".");
+				if (extPos != std::string::npos) {
+					std::string ext = file_a.substr(extPos+1);
+					if (IS_CODE(ext)) {
+						is_code = true;
+						diff_record->set_is_code(ext);
+					}
+				} 
+				if (!is_code) {
+					extPos = file_b.rfind(".");
+					if (extPos != std::string::npos) {
+						std::string ext = file_b.substr(extPos+1);
+						if (IS_CODE(ext)) {
+							is_code = true;
+							diff_record->set_is_code(ext);
+						}
+					}
+				}
+				if (! is_code) 
+					diff_record->set_is_code("");
+				is_special = true;
+				hunk = NULL;
+				hunk_text = "";
+			    }
+			}
+			prefixPos = diff_line.find(INDEX_PREFIX);
+			if (prefixPos != std::string::npos) {
+			    std::string index_prefix= diff_line.substr(0, prefixPos);
+			    if (index_prefix == "") {
+				    // index aa28709..05acaef 100644
+				    std::string index_line = diff_line.substr(prefixPos + strlen(INDEX_PREFIX));
+				    size_t from_Pos = index_line.find("..");
+				    size_t mode_Pos = index_line.find(" ");
+				    if (from_Pos != std::string::npos) {
+					std::string from_id = index_line.substr(0, from_Pos);
+					std::string to_id = "";
+					std::string mode = "";
+					if (mode_Pos == std::string::npos) {
+					    to_id = index_line.substr(from_Pos + 2);
+					} else {
+					    to_id = index_line.substr(from_Pos + 2).substr(0, mode_Pos - from_Pos - 2);
+					    mode = index_line.substr(mode_Pos + 1);
+					}
+					if (from_id == "0000000")
+						diff_record->set_is_new(true);
+					else
+						diff_record->set_is_new(false);
+					diff_record->set_index_from(from_id);
+					diff_record->set_index_to(to_id);
+					diff_record->set_mode(mode);
+				    }
+				is_special = true;
+			    }
+			}
+			prefixPos = diff_line.find(HUNK_PREFIX);
+			if (prefixPos != std::string::npos) {
+			    std::string hunk_prefix= diff_line.substr(0, prefixPos);
+			    if (hunk_prefix == "") {
+				    if (hunk != NULL && hunk_text != "") {
+					if (diff_record->is_code() != "") {
+					    process_hunk_xml(hunk, hunk_text, diff_record->is_code()); 
+					} else {
+					    process_hunk_text(hunk, hunk_text);
+					}
+					hunk = NULL;
+					hunk_text = "";
+				    }
+				    // @@ -4,10 +4,13 @@ BLABLA
+				    std::string hunk_line = diff_line.substr(prefixPos + strlen(HUNK_PREFIX));
+				    size_t from_Pos = hunk_line.find("-");
+				    size_t to_Pos = hunk_line.find("+");
+				    size_t context_Pos = hunk_line.find(HUNK_PREFIX);
+				    hunk = diff_record->add_hunk();
+				    if (from_Pos != std::string::npos) {
+					std::string from_id = hunk_line.substr(from_Pos+1, to_Pos - 1);
+					size_t col_Pos = from_id.find(",");
+					std::string from_lineno = from_id.substr(0, col_Pos);
+					std::string from_column = from_id.substr(col_Pos+1);
+					hunk->set_from_lineno(atoi(from_lineno.c_str()));
+					hunk->set_from_column(atoi(from_column.c_str()));
+				    }
+				    if (to_Pos != std::string::npos) {
+					std::string to_id = hunk_line.substr(to_Pos+1, context_Pos - 1);
+					size_t col_Pos = to_id.find(",");
+					std::string to_lineno = to_id.substr(0, col_Pos);
+					std::string to_column = to_id.substr(col_Pos+1);
+					hunk->set_to_lineno(atoi(to_lineno.c_str()));
+					hunk->set_to_column(atoi(to_column.c_str()));
+				    }
+				    if (context_Pos != std::string::npos) {
+					std::string context = hunk_line.substr(context_Pos + strlen(HUNK_PREFIX));
+					hunk->set_context(context);
+				    }
+				    is_special = true;
+			    }
+			}
+			prefixPos = diff_line.find(SKIP_PREFIX1);
+			if (prefixPos != std::string::npos) {
+			    std::string diff_prefix= diff_line.substr(0, prefixPos);
+			    if (diff_prefix == "") {
+				    is_special = true;
+			    }
+			}
+			prefixPos = diff_line.find(SKIP_PREFIX2);
+			if (prefixPos != std::string::npos) {
+			    std::string diff_prefix= diff_line.substr(0, prefixPos);
+			    if (diff_prefix == "") {
+				    is_special = true;
+			    }
+			}
+			prefixPos = diff_line.find(SKIP_PREFIX3);
+			if (prefixPos != std::string::npos) {
+			    std::string diff_prefix= diff_line.substr(0, prefixPos);
+			    if (diff_prefix == "") {
+				    is_special = true;
+			    }
+			}
+			prefixPos = diff_line.find(SKIP_PREFIX4);
+			if (prefixPos != std::string::npos) {
+			    std::string diff_prefix= diff_line.substr(0, prefixPos);
+			    if (diff_prefix == "") {
+				    is_special = true;
+			    }
+			}
+			if (! is_special)
+				hunk_text += diff_line + "\n";
+		    }
+		    lineno ++;
+		    if (lineno > 100) {
+			    cout << "." << flush;
+			    lineno = 0;
+		    }
+		} while (linePos != std::string::npos);
+		diff = "";
+	}
+}
+
 int main(int argc, char **argv) {
         GOOGLE_PROTOBUF_VERIFY_VERSION;
         fast::Log_Commit * current_commit = NULL;
@@ -167,14 +337,11 @@ int main(int argc, char **argv) {
 	std::string commit_date;
 	std::string commit_email;
 	std::string token;
-	std::string diff_line;
-	std::string file_a;
-	std::string file_b;
 	bool parallel = false;
 	if (strcmp(argv[1], "-p") == 0) {
 		parallel = true;
 		argc-- ;
-		argv = argv + 1;
+		argv++;
 	}
 	long number = 0;
 	int jobs = 1;
@@ -186,7 +353,7 @@ int main(int argc, char **argv) {
 	mkstemp(buf);
 	remove(buf);
 	std::string fn = "/tmp/";
-	fn = fn + buf;
+	fn += buf;
 	remove(fn.c_str());
 	strcat(buf, ".log");
 	string log_filename = buf;
@@ -256,162 +423,8 @@ int main(int argc, char **argv) {
 				no = 0;
 			}
 			no++;
-			if (current_commit != NULL) {
-				// cout << "." << flush;
-				fast::Log_Commit_Diff_Hunk * hunk = NULL;
-				fast::Log_Commit_Diff * diff_record = NULL;
-				size_t linePos = std::string::npos;
-				std::string hunk_text = "";
-				do {
-				    linePos = diff.find("\n");
-				    if (linePos != std::string::npos) {
-					diff_line = diff.substr(0, linePos);
-					diff = diff.substr(linePos + 1);
-					bool is_special = false;
-					size_t prefixPos = diff_line.find(DIFF_PREFIX);
-					if (prefixPos != std::string::npos) {
-					    std::string diff_prefix= diff_line.substr(0, prefixPos);
-					    if (diff_prefix == "") {
-					    	diff_line = diff_line.substr(prefixPos + strlen(DIFF_PREFIX));
-					    	size_t fieldPos = diff_line.find(" b/");
-						file_a = diff_line.substr(2, fieldPos - 2);
-						file_b = diff_line.substr(fieldPos+3);
-						diff_record = current_commit->add_diff();
-						diff_record->set_a(file_a);
-						diff_record->set_b(file_b);
-						bool is_code = false;
-						size_t extPos = file_a.rfind(".");
-						if (extPos != std::string::npos) {
-							std::string ext = file_a.substr(extPos+1);
-							if (IS_CODE(ext)) {
-								is_code = true;
-								diff_record->set_is_code(ext);
-							}
-						} 
-						if (!is_code) {
-							extPos = file_b.rfind(".");
-							if (extPos != std::string::npos) {
-								std::string ext = file_b.substr(extPos+1);
-								if (IS_CODE(ext)) {
-									is_code = true;
-									diff_record->set_is_code(ext);
-								}
-							}
-						}
-						if (! is_code) 
-							diff_record->set_is_code("");
-					    	is_special = true;
-						hunk = NULL;
-						hunk_text = "";
-					    }
-					}
-					prefixPos = diff_line.find(INDEX_PREFIX);
-					if (prefixPos != std::string::npos) {
-					    std::string index_prefix= diff_line.substr(0, prefixPos);
-					    if (index_prefix == "") {
-						    // index aa28709..05acaef 100644
-					    	    std::string index_line = diff_line.substr(prefixPos + strlen(INDEX_PREFIX));
-						    size_t from_Pos = index_line.find("..");
-						    size_t mode_Pos = index_line.find(" ");
-						    if (from_Pos != std::string::npos) {
-							std::string from_id = index_line.substr(0, from_Pos);
-						   	std::string to_id = "";
-						   	std::string mode = "";
-							if (mode_Pos == std::string::npos) {
-							    to_id = index_line.substr(from_Pos + 2);
-							} else {
-							    to_id = index_line.substr(from_Pos + 2).substr(0, mode_Pos - from_Pos - 2);
-							    mode = index_line.substr(mode_Pos + 1);
-							}
-							if (from_id == "0000000")
-								diff_record->set_is_new(true);
-							else
-								diff_record->set_is_new(false);
-							diff_record->set_index_from(from_id);
-							diff_record->set_index_to(to_id);
-							diff_record->set_mode(mode);
-						    }
-					    	is_special = true;
-					    }
-					}
-					prefixPos = diff_line.find(HUNK_PREFIX);
-					if (prefixPos != std::string::npos) {
-					    std::string hunk_prefix= diff_line.substr(0, prefixPos);
-					    if (hunk_prefix == "") {
-						    if (hunk != NULL && hunk_text != "") {
-							if (diff_record->is_code() != "") {
-							    process_hunk_xml(hunk, hunk_text, diff_record->is_code()); 
-							} else {
-							    process_hunk_text(hunk, hunk_text);
-							}
-							hunk = NULL;
-							hunk_text = "";
-						    }
-						    // @@ -4,10 +4,13 @@ BLABLA
-					    	    std::string hunk_line = diff_line.substr(prefixPos + strlen(HUNK_PREFIX));
-						    size_t from_Pos = hunk_line.find("-");
-						    size_t to_Pos = hunk_line.find("+");
-						    size_t context_Pos = hunk_line.find(HUNK_PREFIX);
-						    hunk = diff_record->add_hunk();
-						    if (from_Pos != std::string::npos) {
-							std::string from_id = hunk_line.substr(from_Pos+1, to_Pos - 1);
-							size_t col_Pos = from_id.find(",");
-						   	std::string from_lineno = from_id.substr(0, col_Pos);
-						   	std::string from_column = from_id.substr(col_Pos+1);
-							hunk->set_from_lineno(atoi(from_lineno.c_str()));
-							hunk->set_from_column(atoi(from_column.c_str()));
-						    }
-						    if (to_Pos != std::string::npos) {
-							std::string to_id = hunk_line.substr(to_Pos+1, context_Pos - 1);
-							size_t col_Pos = to_id.find(",");
-						   	std::string to_lineno = to_id.substr(0, col_Pos);
-						   	std::string to_column = to_id.substr(col_Pos+1);
-							hunk->set_to_lineno(atoi(to_lineno.c_str()));
-							hunk->set_to_column(atoi(to_column.c_str()));
-						    }
-						    if (context_Pos != std::string::npos) {
-							std::string context = hunk_line.substr(context_Pos + strlen(HUNK_PREFIX));
-							hunk->set_context(context);
-						    }
-						    is_special = true;
-					    }
-					}
-					prefixPos = diff_line.find(SKIP_PREFIX1);
-					if (prefixPos != std::string::npos) {
-					    std::string diff_prefix= diff_line.substr(0, prefixPos);
-					    if (diff_prefix == "") {
-						    is_special = true;
-					    }
-					}
-					prefixPos = diff_line.find(SKIP_PREFIX2);
-					if (prefixPos != std::string::npos) {
-					    std::string diff_prefix= diff_line.substr(0, prefixPos);
-					    if (diff_prefix == "") {
-						    is_special = true;
-					    }
-					}
-					prefixPos = diff_line.find(SKIP_PREFIX3);
-					if (prefixPos != std::string::npos) {
-					    std::string diff_prefix= diff_line.substr(0, prefixPos);
-					    if (diff_prefix == "") {
-						    is_special = true;
-					    }
-					}
-					prefixPos = diff_line.find(SKIP_PREFIX4);
-					if (prefixPos != std::string::npos) {
-					    std::string diff_prefix= diff_line.substr(0, prefixPos);
-					    if (diff_prefix == "") {
-						    is_special = true;
-					    }
-					}
-					if (! is_special)
-						hunk_text = hunk_text + diff_line + "\n";
-				    }
-				} while (linePos != std::string::npos);
-				diff = "";
-			}
+			commit(current_commit, diff);
 			std::getline(input, commit_id);
-			// std::cout << commit_id << endl << std::flush;
 			std::getline(input, text);
 			std::getline(input, author_name);
 			std::getline(input, author_email);
@@ -434,11 +447,6 @@ int main(int argc, char **argv) {
 		} else {
 			diff += line;
 			diff += "\n";
-			/* lineno++;
-			if (lineno > 10000) {
-				cout << "." << flush;
-				lineno = 0;
-			} */
 		}
 	}
 	remove(log_filename.c_str());
@@ -447,6 +455,7 @@ int main(int argc, char **argv) {
 	else
 		sprintf(filename, "%s.pb", argv[1]);
 	if (no != 0) {
+		commit(current_commit, diff);
 		cout << "saving " << no << " records into " << filename << " ..." << endl;
 		fstream output(filename, ios::out | ios::trunc | ios::binary);
 		log->SerializeToOstream(&output);
